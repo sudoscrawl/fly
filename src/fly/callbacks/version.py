55f543bbb621190702cdd25767112a518717ec04
Ayush Kumar <ayush438500@gmail.com>
2026-07-19 03:27:39 +0530

    feat: add ruff formating support

--- src/fly/callbacks/version.py
+++ src/fly/callbacks/version.py
@@ -1,5 +1,6 @@
 import typer
 from fly import __version__
 
+
 def get_version():
     typer.echo(f"fly version {__version__}")
--- src/fly/main.py
+++ src/fly/main.py
@@ -3,17 +3,13 @@ from fly.callbacks.version import get_version
 
 app = typer.Typer()
 
+
 @app.callback(invoke_without_command=True)
 def main(
     version: bool = typer.Option(
-            False,
-            "--version",
-            "-v",
-            help= "Print version information",
-            is_eager=True
-        )
+        False, "--version", "-v", help="Print version information", is_eager=True
+    ),
 ):
     if version:
         get_version()
         raise typer.Exit()
-
