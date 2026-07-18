ca980812479ed843780aa667cd0d5cc110dec43f
Ayush Kumar <ayush438500@gmail.com>
2026-07-18 18:53:16 +0530

    feat: basic cli setup

---
+++ src/fly/main.py
@@ -0,0 +1,11 @@
+import typer
+
+app = typer.Typer()
+
+@app.command()
+def hello(name: str) -> None:
+    print(f"Hello {name}")
+
+@app.command()
+def goodbye(name: str) -> None:
+    print(f"Goodbye {name}")
