2fa1323a94ff1de8a77af96dfaa0e225817326fb
Ayush Kumar <ayush438500@gmail.com>
2026-07-20 01:20:44 +0530

    feat: add modifying gitignore

--- src/fly/services/fileio.py
+++ src/fly/services/fileio.py
@@ -6,3 +6,11 @@ class FileIO:
     def create_fly_dir(path: Path):
         fly_dir = path / ".fly"
         fly_dir.mkdir(exist_ok=True)
+
+    @staticmethod
+    def update_gitignore(path: str):
+        gitignore_path = f"{path}/.gitignore"
+        strings = ["# Fly\n", ".fly\n"]
+
+        with open(gitignore_path, "a") as file:
+            file.writelines(strings)
