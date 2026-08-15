33b7ef536763ea9abdc7533e965c18b10d041c46
Ayush Kumar <ayush438500@gmail.com>
2026-08-16 03:07:44 +0530

    fix: naming and other bug fixes to upload to pypi

--- src/fly/__init__.py
+++ src/fly/__init__.py
@@ -1,6 +1,6 @@
 from importlib.metadata import version
 
-__version__ = version("fly")
+__version__ = version("flyt")
 
 # It's this way so that I don't need to manually change the version
 # at every place where it gets used
